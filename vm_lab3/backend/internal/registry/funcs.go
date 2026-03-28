345077335fa75f8d6c954a91a2e6fd2541860884
xtern0o <dos30028@gmail.com>
2026-03-28 12:32:58 +0300

    [feat] simpson method implemented

--- vm_lab3/backend/internal/registry/funcs.go
+++ vm_lab3/backend/internal/registry/funcs.go
@@ -2,17 +2,20 @@ package registry
 
 import "fmt"
 
-var func1 func(x float64) float64 = func(x float64) float64 {
-	return -2*x*x*x - 3*x*x + x + 5
-}
+var (
+	func1 = func(x float64) float64 {
+		return -2*x*x*x - 3*x*x + x + 5
+	}
+)
 
-var funcs []func(x float64) float64 = []func(x float64) float64{
-	func1,
+var id2func = map[int]func(x float64) float64{
+	1: func1,
 }
 
 func GetFunc(id int) (func(x float64) float64, error) {
-	if 0 <= id && id < len(funcs) {
-		return funcs[id], nil
+	fun := id2func[id]
+	if fun == nil {
+		return nil, fmt.Errorf("none of func with id=%d", id)
 	}
-	return nil, fmt.Errorf("none of func with id=%d", id)
+	return fun, nil
 }
